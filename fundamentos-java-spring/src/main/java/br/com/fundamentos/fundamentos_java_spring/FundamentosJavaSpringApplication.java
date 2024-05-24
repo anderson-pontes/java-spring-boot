444d852985422889668cdb4ba238b10257d8d524
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2024-05-24 15:09:04 -0300

    Meu primeiro commit

---
+++ fundamentos-java-spring/src/main/java/br/com/fundamentos/fundamentos_java_spring/FundamentosJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.fundamentos.fundamentos_java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosJavaSpringApplication.class, args);
+	}
+
+}
---
+++ fundamentos-java-spring/src/test/java/br/com/fundamentos/fundamentos_java_spring/FundamentosJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fundamentos.fundamentos_java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
